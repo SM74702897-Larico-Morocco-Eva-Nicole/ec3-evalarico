313ab3a10beeba132110feef223d012006a2c8bb
EvaLM07 <134290501+EvaLM07@users.noreply.github.com>
2023-07-24 15:19:06 +0000

    Commit con Render

---
+++ src/main/java/com/example/ec3evalarico/Ec3EvalaricoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec3evalarico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3EvalaricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3EvalaricoApplication.class, args);
+	}
+
+}
